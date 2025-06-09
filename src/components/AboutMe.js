3e59fd8d2272000f83bd5f9ee3a4a79e2df3839a
divice01 <dmacias49@calstela.edu>
2025-06-09 10:42:14 -0700

    commit

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -6,16 +6,16 @@ const AboutMe = () => {
       <div className={styles.container}>
         <h2 className={styles.title}>About Me</h2>
         <p className={styles.description}>
-          Hi I'm Daniel Macias, a passionate Full-Stack Developer with a
-          Master's degree in Computer Science. I specialize in building dynamic,
+          Hi I am Daniel Macias, a passionate Full-Stack Developer with a
+          Masters degree in Computer Science. I specialize in building dynamic,
           scalable web applications using modern technologies like Next.js,
           Node.js Express.js, PostgreSQL/MongoDB, and NextAuth.js. With a strong
           foundation in computer networking, software engineering, and
           reinforcement learning, I bring a problem-solving mindset to every
           project. Whether optimizing backend APIs or designing intuitive UIs, I
-          thrive on creating high-performance applications. Outside of
-          coding, I enjoy exploring emerging tech trends, refining my skills in
-          machine learning and cloud computing, and contributing to open-source
+          thrive on creating high-performance applications. Outside of coding, I
+          enjoy exploring emerging tech trends, refining my skills in machine
+          learning and cloud computing, and contributing to open-source
           projects.
         </p>
       </div>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,13 +1,15 @@
-import styles from '../styles/Hero.module.css'; // Import the styles
-import { useRouter } from 'next/router';
+import styles from "../styles/Hero.module.css"; // Import the styles
+import { useRouter } from "next/router";
 
 const Hero = () => {
   const router = useRouter();
-  const handleExploreClick = () => {router.push('/projects');}
+  const handleExploreClick = () => {
+    router.push("/projects");
+  };
   return (
     <section className={styles.hero}>
       <div className={styles.container}>
-        <h1>Hi, I'm Daniel. A Passionate Full-Stack Developer.</h1>
+        <h1>Hi, I am Daniel. A Passionate Full-Stack Developer.</h1>
         <p>Building innovative and scalable web applications.</p>
         <button onClick={handleExploreClick}>Explore My Work</button>
       </div>
